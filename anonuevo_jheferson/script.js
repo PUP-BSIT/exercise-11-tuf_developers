ec9ea584c0fb220d8d77e9269febd6d1364d8dc4
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-04-26 20:40:10 +0800

    add the class mode paragraph

--- anonuevo_jheferson/script.js
+++ anonuevo_jheferson/script.js
@@ -1,6 +1,6 @@
 function validateComment() {
     let comment_name = document.getElementById("name");
-    let comment_message = document.getElementById("message");
+    let comment_message = document.getElementById("comment");
     let comment_button = document.getElementById("comment_button");
 
     if (comment_name.value.length < 1 ||
@@ -12,4 +12,5 @@ function validateComment() {
 }
 
 document.getElementById("name").addEventListener("input", validateComment);
-document.getElementById("message").addEventListener("input", validateComment);
+document.getElementById("comment").addEventListener("input", validateComment);
+validateComment();
